1d87953401773a08fec34fdcc1b0686fb5f90bd5
Cecilia <ceciliawzx@qq.com>
2024-03-03 01:58:46 +0000

    add requirements

--- analysis_test.py
+++ analysis_test.py
@@ -1,9 +1,8 @@
 from test_data import users
-from reports_generation import generate_reports, get_report_for_user
-
+from reports_generation import generate_reports, get_report_for_user, get_reports_for_user
 
 reports = generate_reports(users)
-report = get_report_for_user(reports, 'J', 'O')
-
+print(get_reports_for_user(reports, 'A'))
+report = get_report_for_user(reports, 'A', 'D')
 
 print(report)
